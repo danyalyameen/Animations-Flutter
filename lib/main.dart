864ba223af1267bdfec3279e6640732c7aa0e449
Danyal Yameen <muhammaddanyalyameen@gmail.com>
2025-04-14 20:00:03 +0500

    Setup Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/Animations/Implicit%20Animations/implicit_animations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ImplicitAnimations(),
     );
   }
 }
